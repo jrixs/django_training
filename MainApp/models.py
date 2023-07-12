ffa2e0081c267804380a7c1b23e0e9fdda62f8bb
user <jrixjrix@gmail.com>
2023-07-12 19:39:54 +0300

    module4 fix

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Colors(models.Model):
+   name  = models.CharField(max_length=32)
 
+   def __repr__(self):
+      return f'Colors({self.name})'
 
 class Item(models.Model):
    name  = models.CharField(max_length=100)
    brand = models.CharField(max_length=100)
    count = models.PositiveIntegerField()
    description = models.CharField(max_length=1000)
+   colors = models.ManyToManyField(to=Colors)
+
+   def __repr__(self):
+      return f'Item({self.name, self.brand, self.count, self.description})'
